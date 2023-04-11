d648101cc0a5db64f743fbd5b9dcd75ef6a3a4b0
NapatpanOP <napatpanns424@gmail.com>
2023-04-11 20:54:27 +0700

    Nevbar

---
+++ src/components/Nevbar/MenuItems.jsx
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Types of websites",
+    url: "#",
+    cName: "nev-links"
+  },
+  {
+    title: "Graph",
+    url: "#",
+    cName: "nev-links"
+  },
+  {
+    title: "Suggestion",
+    url: "#",
+    cName: "nev-links"
+  },
+  {
+    title: "Log in",
+    url: "#",
+    cName: "nev-links-mobile"
+  }
+]
---
+++ src/components/Nevbar/Nevbar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { MenuItems } from "./MenuItems"
+import { Button } from '../Button'
+import './Nevbar.css'
+
+class Nevbar extends React.Component {
+  state = { clicked: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    return(
+      <nev className="NevbarItems">
+        <img className="nevbar-logo" src="./src/assets/Logo.png" alt="Logo" />
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={this.state.clicked  ? 'nav-menu active' : 'nev-menu'}>
+          { MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+
+        </ul>
+        <Button>Log in</Button>
+      </nev>
+    )
+  }
+}
+
+export default Nevbar
