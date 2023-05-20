7b2d844b45bd94a7c3ab58bb8a04bfdad4b99020
MemoryKE <kittiwat.ek@gmail.com>
2023-05-21 01:02:02 +0700

    data template

---
+++ src/templateData.js
@@ -0,0 +1,19 @@
+// {
+//     "title": "Youtube",
+//     "image": "./src/assets/brand01.jpg", หน้าการ์ด
+//     "full_image": "./src/assets/positionPage/PositionYoutube.jpg" รูปใหญ่ รวมตำแหน่ง
+//     "type": "Portal",
+//     "logo_brand:" "./src/assets/logoBrands/youtube.jpg", logo หน้า position
+//     "like": []
+//     "adsPositions": [
+//         {
+//         "images_urls": [  // หนึ่งก้อนต่อ 1 position  
+//                  { // หนึ่งก้อนต่อ 1 graphic
+//                      "images_url": "./src/assets/GraphicsPositionBrand/Youtube/YTGP1-1.jpg", รูปกราฟิก 
+//                      "selected_counts": 0
+//                  }
+//             ],
+//             "selected_counts": 0
+//         }
+//     ]
+// }
