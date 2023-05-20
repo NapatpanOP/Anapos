bc554deee994100f9c32e88667f04f292270fb5b
NapatpanOP <napatpanns424@gmail.com>
2023-05-21 03:26:44 +0700

    update table

--- src/admin/Conclusion/ConclusionPage.jsx
+++ src/admin/Conclusion/ConclusionPage.jsx
@@ -1,5 +1,38 @@
+import React from 'react';
+import { data } from '../../components/data.json';
+import './ConclusionPage.css';
+
 const ConclusionPage = () => {
-    return <div>conclusion</div>
+    return (
+        <div class="box-table">
+            <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>All Like</th>
+                    <th>Male Like</th>
+                    <th>Female Like</th>
+                    <th>Other Like</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => (
+                    <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.type}</td>
+                    <td>{item.like_all}</td>
+                    <td>{item.like_male}</td>
+                    <td>{item.like_female}</td>
+                    <td>{item.like_other}</td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default ConclusionPage
