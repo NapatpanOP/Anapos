d2cc2258e79571b0ad68e68edfe85ad57a845bca
NapatpanOP <napatpanns424@gmail.com>
2023-05-25 06:57:53 +0700

    update home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,33 +1,89 @@
-import "./HomePage.css"
+import "./HomePage.css";
 import picturRoomGirl from "./assets/Room - Girl Working.png";
 import { useAuthContext } from "./core/contexts/AuthProvider";
+import iconNum1 from "./assets/icon/icon-num1.png";
+import iconNum2 from "./assets/icon/icon-num2.png";
+import iconNum3 from "./assets/icon/icon-num3.png";
+import iconNum4 from "./assets/icon/icon-num4.png";
 
 function HomePage() {
-  const { token } = useAuthContext()
-  
+  const { token } = useAuthContext();
+
   return (
     <div>
       <div className="full-size">
         <div className="box">
-          <p className="box-text1">This is a website that explores the design structure of each website.</p>
+          <p className="box-text1">
+            This is a website that explores the design structure of each
+            website.
+          </p>
           <div class="box-description">
             <p className="box-text2">
-          เว็บแอปพลิเคชั่นเพื่อรวบรวมข้อมูลการวางตำแหน่งโฆษณาสำหรับเว็บไซต์แต่ละประเภท 
-          โดยเรานำเว็บไซต์มาจากการจัดอันดับเว็บไซต์ที่มีผู้เข้าชมมากที่สุดอันดับที่ 1 ถึง 3 ในประเภทสื่อให้ความบันเทิง ข่าวสาร และสินค้าไอที ซึ่งผู้ใช้จะมีส่วนช่วยในการรวบรวมข้อมูลเพื่อหาตำแหน่งการวางโฆษณาจากการเลือกเว็บไซต์ที่ตนเข้าใช้งานเป็นประจำและทำการเลือกตำแหน่งโฆษณาที่ตนรู้สึกสะดุดตามากที่สุด เว็บไซต์จะทำการแสดงข้อมูลที่ผู้ใช้ทั้งหมดเลือกในรูปแบบของกราฟข้อมูล เพื่อให้ผู้ใช้นำข้อมูลไปพัฒนาเว็บไซต์ของตนให้ดียิ่งขึ้น
-          </p>
-          </div> 
-          <button type="button" class="btn btn-success" id="bt-start">Get Started</button>
+              เว็บแอปพลิเคชั่นเพื่อรวบรวมข้อมูลการวางตำแหน่งโฆษณาสำหรับเว็บไซต์แต่ละประเภท
+              โดยเรานำเว็บไซต์มาจากการจัดอันดับเว็บไซต์ที่มีผู้เข้าชมมากที่สุดอันดับที่
+              1 ถึง 3 ในประเภทสื่อให้ความบันเทิง ข่าวสาร และสินค้าไอที
+              ซึ่งผู้ใช้จะมีส่วนช่วยในการรวบรวมข้อมูลเพื่อหาตำแหน่งการวางโฆษณาจากการเลือกเว็บไซต์ที่ตนเข้าใช้งานเป็นประจำและทำการเลือกตำแหน่งโฆษณาที่ตนรู้สึกสะดุดตามากที่สุด
+              เว็บไซต์จะทำการแสดงข้อมูลที่ผู้ใช้ทั้งหมดเลือกในรูปแบบของกราฟข้อมูล
+              เพื่อให้ผู้ใช้นำข้อมูลไปพัฒนาเว็บไซต์ของตนให้ดียิ่งขึ้น
+            </p>
+          </div>
+          <button type="button" class="btn btn-success" id="bt-start">
+            Get Started
+          </button>
         </div>
-        <img src={picturRoomGirl} alt="Image" className="picture-roomGirl"/>
+        <img src={picturRoomGirl} alt="Image" className="picture-roomGirl" />
       </div>
-        
-        <div className="head-web">
-          <p>About the website</p> 
+
+      <div class="box-content-home">
+        <div className="head-text">
+          <p>Steps to use</p>
         </div>
 
-        
+        <div className="card-grid-steps">
+            <div className="card">
+              <img src={iconNum1} alt="icon1" />
+              <div className="card-content">
+                <h3>Step 1</h3>
+                <p>ไปที่หน้า Type of websites</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src={iconNum2} alt="icon2" />
+              <div className="card-content">
+                <h3>Step 2</h3>
+                <p>
+                  กดเข้าเว็บไซต์ที่ใช้งานประจำหรือ<br/>ที่ชอบการวางตำแหน่งโฆษณา<br/>
+                  ตรงนี้ผู้ใช้ยังสามารถกดถูกใจเว็บไซต์ที่ชอบได้
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <img src={iconNum3} alt="icon3" />
+              <div className="card-content">
+                <h3>Step 3</h3>
+                <p>
+                  ดูภาพการวางตำแหน่งโฆษณา จากนั้นกดปุ่มเพื่อเลือกตำแหน่งโฆษณาที่ถูกใจ
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <img src={iconNum4} alt="icon3" />
+              <div className="card-content">
+                <h3>Step 4</h3>
+                <p>
+                  เว็บไซต์จะพาไปยังหน้าเลือกกราฟิกของโฆษณาตำแหน่งที่ผู้ใช้เลือก ดูภาพกราฟิกเพื่อเลือกรูปแบบที่ชอบ
+                  สามารถแสดงความคิดเห็นเกี่ยวกับกราฟิกได้ อันดับสุดท้ายกดปุ่มเพื่อเลือกกราฟิกเป็นอันเสร็จสิ้น
+                </p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div className="head-text">
+          <p>Summary graphs</p>
+      </div>
+
     </div>
-    
   );
 }
 
