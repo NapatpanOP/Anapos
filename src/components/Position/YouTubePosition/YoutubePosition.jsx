6c3a3eb4c201089eed5831e1b7d9dc1d85291381
NapatpanOP <napatpanns424@gmail.com>
2023-06-08 03:07:49 +0700

    update youtubeposition

--- src/components/Position/YouTubePosition/YoutubePosition.jsx
+++ src/components/Position/YouTubePosition/YoutubePosition.jsx
@@ -2,33 +2,39 @@ import React from 'react'
 import "./YoutubePosition.css"
 
 function YoutubePosition() {
+  
   return (
     <div class="full-size-youtube">
-      <div>
-        <div class="box-yt-position1-1"></div>
+      <div class="box-yt">
+        <button class="box-yt-position1-1"></button>
+        {/* <div class="box-yt-position1-1"></div> */}
         <div class="box-yt-position1-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <div class="box-yt-position2-1"></div>
+        <button class="box-yt-position2-1"></button>
+        {/* <div class="box-yt-position2-1"></div> */}
         <div class="box-yt-position2-2"></div>
         <div class="box-yt-position2-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <div class="box-yt-position3-1"></div>
+        <button class="box-yt-position3-1"></button>
+        {/* <div class="box-yt-position3-1"></div> */}
         <div class="box-yt-position3-2"></div>
         <div class="box-yt-position3-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <div class="box-yt-position4-1"></div>
+        <button class="box-yt-position4-1"></button>
+        {/* <div class="box-yt-position4-1"></div> */}
         <div class="box-yt-position4-2"></div>
         <div class="box-yt-position4-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position-end">
-        <div class="box-yt-position5-1"></div>
+        <button class="box-yt-position5-1"></button>
+        {/* <div class="box-yt-position5-1"></div> */}
         <div class="box-yt-position5-2"></div>
         <div class="box-yt-position5-2"></div>
       </div>
