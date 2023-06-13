61913f18db2bbe5fdbc93a91932e78ff127cf4d7
NapatpanOP <napatpanns424@gmail.com>
2023-06-13 17:08:48 +0700

    update dailynews link position

--- src/components/Position/DailynewsPosition/DailynewsPosition.jsx
+++ src/components/Position/DailynewsPosition/DailynewsPosition.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./DailynewsPosition.css"
 
-function DailynewsPosition() {
+function DailynewsPosition({selectPosHandle}) {
   return (
     <div class="full-size-dailynews">
       <div class="head-box-dailynews">
         <div class="box-position-dn1-1"></div>
-        <button class="box-position-dn1"></button>
+        <button class="box-position-dn1" onClick={() => selectPosHandle(0)}></button>
         <div class="box-position-dn1-1"></div>
       </div>
 
@@ -45,7 +45,7 @@ function DailynewsPosition() {
       <div class="box-dailynews-4">
         <h3 class="topic-dailynews-color-5">TOPIC</h3>
         <div class="box-dailynews-4-1">
-          <button class="box-position-dn2"></button>
+          <button class="box-position-dn2" onClick={() => selectPosHandle(1)}></button>
           <div class="box-dailynews-5-2">
             <h5>Title</h5>
             <p>Text</p>
@@ -56,7 +56,7 @@ function DailynewsPosition() {
       <div class="box-dailynews-4">
         <h3 class="topic-dailynews-color-5">TOPIC</h3>
         <div class="box-dailynews-4-1">
-          <button class="box-position-dn3"></button>
+          <button class="box-position-dn3" onClick={() => selectPosHandle(2)}></button>
           <div class="box-position-dn3-1"></div>
           <div class="box-position-dn3-1"></div>
         </div>
@@ -65,14 +65,14 @@ function DailynewsPosition() {
       <div class="box-dailynews-4">
         <h3 class="topic-dailynews-color-5">TOPIC</h3>
         <div class="box-dailynews-4-1">
-          <button class="box-position-dn4"></button>
+          <button class="box-position-dn4" onClick={() => selectPosHandle(3)}></button>
           <div class="box-position-dn3-1"></div>
           <div class="box-position-dn3-1"></div>
         </div>
       </div>
 
       <div class="box-dailynews-5">
-        <button class="box-position-dn5"></button>
+        <button class="box-position-dn5" onClick={() => selectPosHandle(4)}></button>
         <h4 class="topic-daiynews-5">TOPIC</h4>
       </div>
 
@@ -80,7 +80,7 @@ function DailynewsPosition() {
 
       <div class="box-dailynews-7">
         <div class="box-dailynews-3-1">
-          <button class="box-position-dn6"></button>
+          <button class="box-position-dn6" onClick={() => selectPosHandle(5)}></button>
           <div class="box-position-dn6-1"></div>
           <div class="box-position-dn6-1"></div>
           <div class="box-position-dn6-1"></div>
@@ -90,7 +90,7 @@ function DailynewsPosition() {
       <div class="box-dailynews-8">
         <h3 class="topic-dailynews-3">TOPIC</h3>
         <div class="box-dailynews-3-1">
-          <button class="box-position-dn7"></button>
+          <button class="box-position-dn7" onClick={() => selectPosHandle(6)}></button>
           <div class="box-position-dn6-1"></div>
           <div class="box-position-dn6-1"></div>
           <div class="box-position-dn6-1"></div>
