9f4c1e83ab1c6f47e965a4d8bdc7d8bf92f921e4
NapatpanOP <napatpanns424@gmail.com>
2023-06-14 14:07:43 +0700

    update jib position page

--- src/components/Position/JibPosition/JibPosition.jsx
+++ src/components/Position/JibPosition/JibPosition.jsx
@@ -4,7 +4,18 @@ import "./JibPosition.css"
 function JibPosition() {
   return (
     <div class="full-size-jib">
-      
+      <div class="box-jib1">
+        <div class="jb-positon1-1"></div>
+        <button class="jb-positon1"></button>
+        <button class="jb-positon2"></button>
+        <div class="box-jib1-1">
+          <button class="jb-positon3"></button>
+          <div class="jb-positon3-1"></div>
+        </div>
+      </div>
+
+      <div class="jb-positon3-2"></div>
+
     </div>
   )
 }
