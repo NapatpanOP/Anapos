04b3306fdcbe571c95fb490b8214573011893ed9
NapatpanOP <napatpanns424@gmail.com>
2023-06-25 03:34:24 +0700

    update boutton active code

--- src/components/Position/YouTubePosition/YoutubePosition.jsx
+++ src/components/Position/YouTubePosition/YoutubePosition.jsx
@@ -6,34 +6,42 @@ function YoutubePosition({selectPosHandle, currentPositionIndex}) {
     <div class="full-size-youtube">
       <div  className="box-yt">
         <button class={[`box-yt-position1-1  ${
-              currentPositionIndex == 0 ? "class-active" : "class-normal"
+              currentPositionIndex == 0 ? "box-yt-position1-1-at" : "box-yt-position1-1"
             }`]} onClick={() => selectPosHandle(0)}></button>
         <div class="box-yt-position1-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <button class="box-yt-position2-1" onClick={() => selectPosHandle(1)}></button>
+        <button class={[`box-yt-position2-1  ${
+              currentPositionIndex == 1 ? "box-yt-position2-1-at" : "box-yt-position2-1"
+            }`]}  onClick={() => selectPosHandle(1)}></button>
         <div class="box-yt-position2-2"></div>
         <div class="box-yt-position2-2"></div>
         <div class="box-yt-position2-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <button class="box-yt-position3-1" onClick={() => selectPosHandle(2)}></button>
+        <button class={[`box-yt-position3-1  ${
+              currentPositionIndex == 2 ? "box-yt-position3-1-at" : "box-yt-position3-1"
+            }`]} onClick={() => selectPosHandle(2)}></button>
         <div class="box-yt-position3-2"></div>
         <div class="box-yt-position3-2"></div>
         <div class="box-yt-position3-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position">
-        <button class="box-yt-position4-1" onClick={() => selectPosHandle(3)}></button>
+        <button class={[`box-yt-position4-1  ${
+              currentPositionIndex == 3 ? "box-yt-position4-1-at" : "box-yt-position4-1"
+            }`]}  onClick={() => selectPosHandle(3)}></button>
         <div class="box-yt-position4-2"></div>
         <div class="box-yt-position4-2"></div>
         <div class="box-yt-position4-2"></div>
       </div>
       <h3 class="topic-yt">TOPIC</h3>
       <div class="box-yt-position-end">
-        <button class="box-yt-position5-1" onClick={() => selectPosHandle(4)}></button>
+        <button class={[`box-yt-position5-1  ${
+              currentPositionIndex == 4 ? "box-yt-position5-1-at" : "box-yt-position5-1"
+            }`]} onClick={() => selectPosHandle(4)}></button>
         <div class="box-yt-position5-2"></div>
         <div class="box-yt-position5-2"></div>
         <div class="box-yt-position5-2"></div>
