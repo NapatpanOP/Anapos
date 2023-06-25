1c95219bd937209cf63dad3034ff363f8bc12a92
NapatpanOP <napatpanns424@gmail.com>
2023-06-25 20:54:44 +0700

    update sugpage

--- src/SugPage.jsx
+++ src/SugPage.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import './SugPage.css';
-import { SuggestionAPI } from './apis/suggestion/SuggestionAPI';
-import { useAuthContext } from './core/contexts/AuthProvider';
+import React, { useState } from "react";
+import "./SugPage.css";
+import { SuggestionAPI } from "./apis/suggestion/SuggestionAPI";
+import { useAuthContext } from "./core/contexts/AuthProvider";
 // import { database } from './firebase';
 
 function SugPage() {
-const { token, loadingAction } = useAuthContext()
+  const { token, loadingAction } = useAuthContext();
 
-  const [websiteName, setWebsiteName] = useState('');
-  const [websiteType, setWebsiteType] = useState('');
-  const [note, setNote] = useState('');
-  const [websiteUrl, setWebsiteUrl] = useState('');
-  const [addStatus, setAddStatus] = useState()
+  const [websiteName, setWebsiteName] = useState("");
+  const [websiteType, setWebsiteType] = useState("");
+  const [note, setNote] = useState("");
+  const [websiteUrl, setWebsiteUrl] = useState("");
+  const [addStatus, setAddStatus] = useState();
 
   const handleNameChange = (event) => {
     setWebsiteName(event.target.value);
@@ -33,7 +33,7 @@ const { token, loadingAction } = useAuthContext()
     event.preventDefault();
 
     // Add website suggestion to database
-    const suggestionsRef = database.ref('suggestions');
+    const suggestionsRef = database.ref("suggestions");
     const newSuggestionRef = suggestionsRef.push();
     newSuggestionRef.set({
       websiteName: websiteName,
@@ -43,51 +43,92 @@ const { token, loadingAction } = useAuthContext()
     });
 
     // Clear form inputs
-    setWebsiteName('');
-    setWebsiteType('Portal');
-    setNote('');
-    setWebsiteUrl('');
+    setWebsiteName("");
+    setWebsiteType("Portal");
+    setNote("");
+    setWebsiteUrl("");
   };
 
-  const addSuggestion = async ()  => {
-    setAddStatus(null)
-    loadingAction.onLoading(true)
+  // const addSuggestion = async () => {
+  //   setAddStatus(null);
+  //   loadingAction.onLoading(true);
+  //   await SuggestionAPI.add({
+  //     user_id: token._id,
+  //     web_title: websiteName,
+  //     type_of_web: websiteType,
+  //     description: note,
+  //     url_of_web: websiteUrl,
+  //   }).then(() => {
+  //     loadingAction.onLoading(false);
+  //   });
+
+  //   setAddStatus("เสร็จสิ้น");
+  // };
+
+  const addSuggestion = async () => {
+    if (!websiteName || !websiteType || !websiteUrl) {
+      // ถ้าข้อมูลไม่ครบถ้วน ไม่ส่งข้อมูล
+      setAddStatus(<span className="error-message">กรุณากรอกข้อมูลให้ครบถ้วน</span>);
+      return;
+    }
+
+    setAddStatus("");
+    loadingAction.onLoading(true);
     await SuggestionAPI.add({
       user_id: token._id,
       web_title: websiteName,
       type_of_web: websiteType,
       description: note,
-      url_of_web:  websiteUrl
+      url_of_web: websiteUrl,
     }).then(() => {
-      loadingAction.onLoading(false)
-    })
-    
-    setAddStatus('success')
+      loadingAction.onLoading(false);
+    });
 
-  }
+    setAddStatus("เสร็จสิ้น");
+
+    // เครียร์ข้อมูลหลังจากกดส่ง
+    setWebsiteName("");
+    setWebsiteType("");
+    setNote("");
+    setWebsiteUrl("");
+
+    // เคลียร์สถานะ
+    setTimeout(() => {
+      setAddStatus(null);
+    }, 2000);
+  };
 
   const renderAddStatus = () => {
-    if(addStatus != null) {
-      return <div className='add-status'>{addStatus}</div>
+    if (addStatus != null) {
+      return <div className="add-status">{addStatus}</div>;
     }
-  }
+  };
 
   return (
     <div className="sug-page">
       <div className="sug-form">
-        
-        <div id='div-form'>
+        <div id="div-form">
           <div className="sug-header">
             <h1>ข้อเสนอแนะ</h1>
           </div>
           <div className="form-group">
             <label htmlFor="website-name">ชื่อเว็บไซต์*:</label>
-            <input type="text" id="website-name" value={websiteName} onChange={handleNameChange} required />
+            <input
+              type="text"
+              id="websiteName"
+              value={websiteName}
+              onChange={handleNameChange}
+              required
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="website-type">ประเภทเว็บไซต์*:</label>
-            <select id="website-type" value={websiteType} onChange={handleTypeChange}>
+            <select
+              id="websiteType"
+              value={websiteType}
+              onChange={handleTypeChange}
+            >
               <option value="">-- ประเภทเว็บไซต์ --</option>
               <option value="Portal">บันเทิง</option>
               <option value="New">ข่าวสาร</option>
@@ -97,16 +138,40 @@ const { token, loadingAction } = useAuthContext()
 
           <div className="form-group">
             <label htmlFor="note">เหตุผลที่เสนอเว็บไซต์นี้:</label>
-            <textarea className="note-form" id="note" value={note} onChange={handleNoteChange}></textarea>
+            <textarea
+              className="note-form"
+              id="note"
+              value={note}
+              onChange={handleNoteChange}
+            ></textarea>
           </div>
 
           <div className="form-group">
             <label htmlFor="website-url">URL ของเว็บไซต์*:</label>
-            <input className="url-form" type="url" id="website-url" value={websiteUrl} onChange={handleUrlChange} required />
+            <input
+              className="url-form"
+              type="url"
+              id="websiteUrl"
+              value={websiteUrl}
+              onChange={handleUrlChange}
+              required
+            />
           </div>
 
           {renderAddStatus()}
-          <button type="submit-sug" className='submit-sug' onClick={() => addSuggestion()}>ยืนยัน</button>
+          <button
+            type="submit-sug"
+            className="submit-sug"
+            onClick={() =>
+              addSuggestion({
+                websiteName: websiteName,
+                websiteType: websiteType,
+                websiteUrl: websiteUrl,
+              })
+            }
+          >
+            ยืนยัน
+          </button>
         </div>
       </div>
     </div>
